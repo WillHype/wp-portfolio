d3d1506ab6ab759ef8c5fab0b6018d8018284605
WillHype <WillHype@users.noreply.github.com>
2023-10-07 22:26:32 +0200

    Ajout d'icones pour les technos

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -3,9 +3,28 @@ import './works.css';
 import projectData from '../../data/projectsData.json';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { faHtml5, faCss3, faJs, faSass } from "@fortawesome/free-brands-svg-icons";
 
 
 const Works = () => {
+    
+    const getIconForTechnology = (technology) => {
+        switch (technology) {
+            case "HTML":
+                return <FontAwesomeIcon icon={faHtml5} />;
+            case "CSS":
+                return <FontAwesomeIcon icon={faCss3} />;
+            case "JavaScript":
+                return <FontAwesomeIcon icon={faJs} />;
+            case "Sass":
+                return <FontAwesomeIcon icon={faSass} />;
+            
+            default:
+                return null;
+        }
+    };
+    
+
     return(
         <section id="works">
             <h2 className="worksTitle">Mon portfolio</h2>
@@ -18,7 +37,17 @@ const Works = () => {
                     <div className="layer">
                         <h3>{project.name}</h3>
                         <p>{project.description}</p>
-                        <a href={project.demoLink}><FontAwesomeIcon icon={faArrowUpRightFromSquare} style={{color: "#7451eb",}} /></a>
+                        <div className="technologies">
+                        <h4>Technologies :</h4>
+                        <div className="technology-icons">
+                            {project.technologies.map((tech, techIndex) => (
+                                <span key={techIndex}>
+                                    {getIconForTechnology(tech)} {tech}
+                                </span>
+                            ))}
+                        </div>
+                    </div>
+                    <a href={project.demoLink}><FontAwesomeIcon icon={faArrowUpRightFromSquare} style={{color: "#7451eb",}} /></a>
                     </div>
                 </div>
         ))}
