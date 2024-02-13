eec59010f3b06a8a001c795594628adc414aed69
WillHype <WillHype@users.noreply.github.com>
2024-02-13 11:49:00 +0100

    Delete a repetitive button

--- src/components/Hero/hero.js
+++ src/components/Hero/hero.js
@@ -1,20 +1,16 @@
 import React from "react";
 import './hero.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
+
 
 
 const Hero = () => {
-    const redirection = () => {
-        window.open("https://www.linkedin.com/in/willy-pochot-a48678297/" , "_blank");
-      };
+
     return(
         <section id="hero">
             <div className="heroContent">
                 <span className="hello">Bonjour,</span>
                 <span className="introText">Je suis <span className="introName">Willy</span> <br />Développeur Front-end</span>
                 <p className="introPara">Je suis développeur Front-end junior basé en île-de-France ayant <br /> de l'expérience en Design et Web</p>
-                <button className="btn" onClick={redirection}><FontAwesomeIcon icon={faBriefcase} className="btnImg"/>Discutons</button>
             </div>
         </section>
     )
